23ff4148516dda372a7f51639b6db005111a2800
ken <ccnhs_cubilo0098@calbayog.ph.education>
2024-09-20 14:55:28 +0800

    added frontpage background

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,11 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">PaBOOK!</span>
+        <div>
+          <img src="/bastaPaBOOK.png" alt="Logo" className="logo" />
+          <span className="pabook">    PaBOOK!</span>
+        </div>
+
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
